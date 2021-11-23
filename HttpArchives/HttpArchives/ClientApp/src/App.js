05945cf8959bc9c0e460178e9028f14f8f93cc8a
Kristiyan Aleksandrov <k.aleksandrov@draftkings.com>
2021-11-23 21:51:04 +0200

    Implement basic file system

--- HttpArchives/HttpArchives/ClientApp/src/App.js
+++ HttpArchives/HttpArchives/ClientApp/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-import { Home } from './components/Home';
+import  Home  from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
-
+import '@progress/kendo-theme-default/dist/all.css';
 import './custom.css'
 
 export default class App extends Component {
--- HttpArchives/HttpArchives/ClientApp/src/components/Home.js
+++ HttpArchives/HttpArchives/ClientApp/src/components/Home.js
@@ -1,26 +1,95 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import * as ReactDOM from "react-dom";
+import {
+    TreeView,
+    processTreeViewItems,
+    handleTreeViewCheckChange,
+} from "@progress/kendo-react-treeview";
+const tree = [
+    {
+        text: "Furniture",
+        items: [
+            {
+                text: "Tables & Chairs",
+            },
+            {
+                text: "Sofas",
+            },
+            {
+                text: "Occasional Furniture",
+                items: [
+                    {
+                        text: "Tables & Chairs",
+                    },
+                    {
+                        text: "Tables & Chairs",
+                    },
+                ]
+            },
+        ],
+    },
+    {
+        text: "Decor",
+        items: [
+            {
+                text: "Bed Linen",
+            },
+            {
+                text: "Curtains & Blinds",
+            },
+            {
+                text: "Carpets",
+            },
+        ],
+    },
+];
 
-export class Home extends Component {
-  static displayName = Home.name;
+const Home = () => {
+    const [check, setCheck] = useState([]);
+    const [expand, setExpand] = useState({
+        ids: ["Item2"],
+        idField: "text",
+    });
+    const [select, setSelect] = useState([""]);
 
-  render () {
+    const onItemClick = (event) => {
+        setSelect([event.itemHierarchicalIndex]);
+    };
+
+    const onExpandChange = (event) => {
+        let ids = expand.ids.slice();
+        const index = ids.indexOf(event.item.text);
+        index === -1 ? ids.push(event.item.text) : ids.splice(index, 1);
+        setExpand({
+            ids,
+            idField: "text",
+        });
+    };
+
+    const onCheckChange = (event) => {
+        const settings = {
+            singleMode: false,
+            checkChildren: false,
+            checkParents: false,
+        };
+        setCheck(handleTreeViewCheckChange(event, check, tree, settings));
+    };
+
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
+        <TreeView
+            data={processTreeViewItems(tree, {
+                select: select,
+                check: check,
+                expand: expand,
+            })}
+            expandIcons={true}
+            onExpandChange={onExpandChange}
+            aria-multiselectable={true}
+            onItemClick={onItemClick}
+            checkboxes={true}
+            onCheckChange={onCheckChange}
+        />
     );
-  }
-}
+};
+
+export default Home;
