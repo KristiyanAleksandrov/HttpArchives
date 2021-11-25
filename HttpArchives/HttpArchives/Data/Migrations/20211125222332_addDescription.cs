9ee5f6171a6693a1111e1b3e5e18d4af88b73aab
Kristiyan Aleksandrov <k.aleksandrov@draftkings.com>
2021-11-26 00:24:28 +0200

    Change db model - add description to har file

---
+++ HttpArchives/HttpArchives/Data/Migrations/20211125222332_addDescription.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HttpArchives.Data.Migrations
+{
+    public partial class addDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "HarFiles",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ParentId",
+                table: "Folders",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Folders_ParentId",
+                table: "Folders",
+                column: "ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Folders_Folders_ParentId",
+                table: "Folders",
+                column: "ParentId",
+                principalTable: "Folders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Folders_Folders_ParentId",
+                table: "Folders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Folders_ParentId",
+                table: "Folders");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "HarFiles");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ParentId",
+                table: "Folders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
