665ec99bda29767af6fc9fd30f4ae75cef5cf909
Kristiyan Aleksandrov <k.aleksandrov@draftkings.com>
2021-11-26 00:43:50 +0200

    Add base controllers

---
+++ HttpArchives/HttpArchives/Controllers/FolderController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HttpArchives.Controllers
+{
+    public class FolderController : ControllerBase
+    {
+    }
+}
---
+++ HttpArchives/HttpArchives/Controllers/HarFileController.cs
@@ -0,0 +1,22 @@
+﻿using HttpArchives.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HttpArchives.Controllers
+{
+    public class HarFileController : ControllerBase
+    {
+        private readonly IHarFileService harFileService;
+
+        public HarFileController(IHarFileService harFileService)
+        {
+            this.harFileService = harFileService;
+        }
+
+        [HttpPost]
+        public async Task Create(string content, string description)
+        {
+            await harFileService.CreateHarFileAsync(content, description);
+        }
+    }
+}
