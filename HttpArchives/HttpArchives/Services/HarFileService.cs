937f614707578f60b326041cad61752161c07b51
Kristiyan Aleksandrov <k.aleksandrov@draftkings.com>
2021-11-26 00:43:24 +0200

    Add Service layer

---
+++ HttpArchives/HttpArchives/Services/HarFileService.cs
@@ -0,0 +1,29 @@
+﻿using HttpArchives.Data;
+using HttpArchives.Models;
+using System.Threading.Tasks;
+
+namespace HttpArchives.Services
+{
+    public class HarFileService : IHarFileService
+    {
+        private readonly ApplicationDbContext db;
+
+        public HarFileService(ApplicationDbContext applicationDbContext)
+        {
+            this.db = applicationDbContext;
+        }
+        public async Task<int> CreateHarFileAsync(string content, string description)
+        {
+            var harFile = new HarFile()
+            {
+                Content = content,
+                Description = description
+            };
+
+            await db.HarFiles.AddAsync(harFile);
+            await db.SaveChangesAsync();
+
+            return harFile.Id;
+        }
+    }
+}
---
+++ HttpArchives/HttpArchives/Services/IHarFileService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace HttpArchives.Services
+{
+    public interface IHarFileService
+    {
+        Task<int> CreateHarFileAsync(string content, string description);
+    }
+}
