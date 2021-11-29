fc32c31bf34929ac2a1d279031b9f22078b8041e
Kristiyan Aleksandrov <k.aleksandrov@draftkings.com>
2021-11-29 16:36:48 +0200

    Revert changes

---
+++ HttpArchives/HttpArchives/Models/Folder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HttpArchives.Models
+{
+    public class Folder
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public Folder Parent { get; set; }
+
+        public IEnumerable<Folder> Children { get; set; }
+
+        public IEnumerable<HarFile> HarFiles { get; set; }
+    }
+}
